ab45bd1b38607e1bd6b2fd20e4084289c247f1c2
LHF <2385107101@qq.com>
2024-08-15 15:49:07 +0800

    Cookies配置

--- src/main/java/com/lhf/usercenter/config/WebMvcConfig.java
+++ src/main/java/com/lhf/usercenter/config/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package com.lhf.usercenter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,9 +15,25 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://139.159.143.140:8000")
+                .allowedOrigins("http://139.159.143.140:8000", "http://meetfei.cn:8000", "http://www.meetfei.cn:8000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    /**
+     * Cookie 配置
+     * @return CookieSerializer
+     */
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setDomainName(".meetfei.cn"); // 修改为实际域名
+        serializer.setCookieName("SESSIONID");
+        serializer.setCookieMaxAge(3600);
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setUseSecureCookie(true); // For HTTPS
+        serializer.setSameSite("None"); // For cross-site requests
+        return serializer;
     }
 }
